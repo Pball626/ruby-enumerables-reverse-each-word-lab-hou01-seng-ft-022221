892eaa44601f2b7bd388d6952059616bf65db464
Jose Mendoza <pball626@gmail.com>
2021-02-11 23:27:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,13 @@
 require "pry"
+<<<<<<< HEAD
 def reverse_each_word(sentence1)
   sentence1 = [" "]
   binding.pry
+=======
+sentence1 = ["Hello there, and how are you?"]
+def reverse_each_word(sentence1)
+  sentence1 = [" "]
+  sentence1.each { |s| sentence1.reverse_each }
+>>>>>>> 424a198b4ca1c9e05b006ef99f9541c96e0f9376
 end
   
